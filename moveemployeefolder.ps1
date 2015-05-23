2c429d43af4373ea460e8985e6a04ec5bb5e2a88
Ingwellm <michael_ingwell@hotmail.com>
2015-05-23 14:22:30 -0700

    Added employee folder move

---
+++ moveemployeefolder.ps1
@@ -0,0 +1,14 @@
+﻿#Move employee documents to supervisor folder
+
+#Get username 
+$employee = raw-input "Please enter employee login name "
+
+#Set location for redirected folders
+$DocumentLocation = 'd:\' #Change to correct location
+
+#Get supervisor username
+$SupervisorName = raw-input "Please enter supervisor login name "
+
+#Copy employee folder to supervisor folder
+copy-item $DocumentLocation\$employee -Destination $DocumentLocation\$SupervisorName
+
